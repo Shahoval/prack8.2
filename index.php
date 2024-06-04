f765c762e0729e00c92ef05c0d27899b578d5a11
Pavlo Shahoval <pavloshahoval06@gmail.com>
2024-06-04 21:02:17 +0300

    fffff

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="index1.php" method="get"
+    style="display: flex; flex-direction: column; width: 25%; margin: auto; text-align: center;">
+        <label>Перша цифра</label>
+        <input type="number" name="firstnumber" required>
+        <label>Друга цифра</label>
+        <input type="number" name="secondnumber" required>
+        <button style="margin-top: 5%;" type="submit">Відправити</button>
+    </form>
+</body>
+</html>
